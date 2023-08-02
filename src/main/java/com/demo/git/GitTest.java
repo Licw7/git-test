5e74b7e7fe2296a38d2643a3d321fd61edb113f3
Licw <372089757@qq.com>
2023-08-02 14:49:01 +0800

    second commit

--- src/main/java/com/demo/git/GitTest.java
+++ src/main/java/com/demo/git/GitTest.java
@@ -4,5 +4,6 @@ package com.demo.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
